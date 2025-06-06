d3b65faeec75034954b5473206ff226567abcc1e
Hejinsd <112409349+Hejinsd@users.noreply.github.com>
2025-06-06 21:06:39 +0800

    Update vue-books-project.sql

--- 图书管理系统数据库/back/vue-books-project.sql
+++ 图书管理系统数据库/back/vue-books-project.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 80026
- Source Host           : localhost:3306
- Source Schema         : vue-books-project
-
- Target Server Type    : MySQL
- Target Server Version : 80026
- File Encoding         : 65001
-
- Date: 05/06/2025 12:00:00
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
