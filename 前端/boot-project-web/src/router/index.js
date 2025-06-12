164235db7ae2db55764a3b8679a0ee29b2f0dc32
gongzhiwei2003 <2044837940@qq.com>
2025-06-12 21:40:59 +0800

    Update index.js

--- 前端/boot-project-web/src/router/index.js
+++ 前端/boot-project-web/src/router/index.js
@@ -2,6 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
 export const constantRoutes = [
   {
     path: '/login',
@@ -13,8 +41,9 @@ export const constantRoutes = [
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
-  }
-    {
+  },
+
+  {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
@@ -32,7 +61,7 @@ export const constantRoutes = [
  * the routes that need to be dynamically loaded based on user roles
  */
 export const asyncRoutes = [
-// {
+  // {
   //   path: '/system',
   //   component: Layout,
   //   alwaysShow: true,
@@ -59,7 +88,21 @@ export const asyncRoutes = [
   //     }
   //   ]
   // },
-
+  // {
+  //   path: '/reader',
+  //   component: Layout,
+  //   name: 'reader',
+  //   alwaysShow: true,
+  //   meta: { title: '读者管理', icon: 'el-icon-s-help' },
+  //   children: [
+  //     {
+  //       path: '/readerList',
+  //       name: 'readerList',
+  //       component: () => import('@/views/reader/readerList'),
+  //       meta: { title: '读者列表', icon: 'table' }
+  //     }
+  //   ]
+  // },
   // {
   //   path: '/book',
   //   component: Layout,
@@ -126,7 +169,6 @@ export const asyncRoutes = [
   // // 404 page must be placed at the end !!!
   // { path: '*', redirect: '/404', hidden: true }
 ]
-
 
 const createRouter = () => new Router({
   // mode: 'history', // require service support
